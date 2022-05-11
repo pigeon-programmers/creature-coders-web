71fdcfd86324b7950e3835a30bb4db684ad51d01
Kendall Perry <perry.kendall@gmail.com>
2022-05-11 10:51:10 -0400

    sign in and sign up (with email!) working!!

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
-  name: {
+  username: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false
