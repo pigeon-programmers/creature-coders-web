ea8389f288584f5b785906b83e137ae4fb6d5dcc
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-05-12 18:11:53 -0400

    Practice loop workflow

--- client/components/Games/Level0/Game01.js
+++ client/components/Games/Level0/Game01.js
@@ -5,7 +5,8 @@ import Interpreter from 'js-interpreter';
 //useRef
 
 export const Game01 = () => {
-  const [string, setString] = useState('');
+  const [string, setString] = useState('EDIT TEXT');
+  const [timesRan, setTimesRan] = useState(0);
 
   useEffect(() => {
     if (string !== 'EDIT TEXT') outcome();
@@ -22,6 +23,19 @@ export const Game01 = () => {
       },
       {
         kind: 'block',
+        type: 'controls_repeat',
+        message0: 'repeat %1 times',
+        args0: [
+          { type: 'field_number', name: 'TIMES', value: '5', check: 'Number' },
+        ],
+        message1: 'do %1',
+        args1: [{ type: 'input_statement', name: 'DO' }],
+        previousStatement: null,
+        nextStatement: null,
+        colour: 120,
+      },
+      {
+        kind: 'block',
         type: 'text',
         fields: { TEXT: 'EDIT TEXT' },
       },
@@ -30,12 +44,14 @@ export const Game01 = () => {
 
   const initApi = (interpreter, scope) => {
     // Add an API function for the alert() block.
-    const wrapper = function (text) {
-      text = text ? text.toString() : '';
-      // const test = document.getElementById('test');
-      // test.innerHTML = text;
-      setString(text);
+    //for loop keep counter variable and once it gets to a certain # then set State
+    let counter = 0;
 
+    const wrapper = function () {
+      // text = text ? text.toString() : '';
+      setTimesRan(++counter);
+      console.log('COUNTER:', counter);
+      console.log('TIMES RAN:', timesRan);
       // original line below. createPrimitive throwing an error so we adjusted it because 🙄
       // return interpreter.createPrimitive(alert(text))
     };
@@ -49,18 +65,26 @@ export const Game01 = () => {
 
   const onRun = (javascriptCode) => {
     const myInterpreter = new Interpreter(javascriptCode, initApi);
-    myInterpreter.run();
+    // myInterpreter.run();
     //IF we want to step through a loop use below instead of run
     //INSTEAD of interpreting code above, have useSTate that is runCode setRunCode that starts off as false and once it starts running it sets state
     // to true, that can then get picked up in useEffect & myInt would go into nextStep.
     //ELSE try useState for interpreter start as null and in useEffect update we can check if null
-
-    // function nextStep() {
-    //   if (myInterpreter.step()) {
-    //     window.setTimeout(nextStep, 0);
-    //   }
-    // }
-    // nextStep();
+    //nextStep below should be in useEffect
+    function nextStep() {
+      if (myInterpreter.step()) {
+        window.setTimeout(nextStep, 50);
+      } else {
+        //maybe put this in a completed state checks on new render
+        if (timesRan === 10) {
+          alert('good job');
+        } else {
+          alert('bad job');
+        }
+        setTimesRan(0);
+      }
+    }
+    nextStep();
   };
 
   //for this ternary we would need to make sure the instructions say to write hello pigeons with no caps or punctuation
@@ -76,6 +100,8 @@ export const Game01 = () => {
         );
   };
 
+  console.log('RENDERING COMPONENT:', timesRan);
+
   return (
     <div style={{ margin: 100 }}>
       <div
@@ -85,7 +111,7 @@ export const Game01 = () => {
           backgroundColor: '#add8e6',
         }}
       >
-        <p id="test">{string}</p>
+        <p id="test">{timesRan}</p>
       </div>
       <Workspace toolbox={toolbox} onRun={onRun} />
     </div>
