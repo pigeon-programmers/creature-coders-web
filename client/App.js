52cebde4667ef547a79bececef19850b2959075b
Kendall Perry <perry.kendall@gmail.com>
2022-05-16 14:41:16 -0400

    removing theme provider file

--- client/App.js
+++ client/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
-import Theme from './components/style/theme';
 
 const App = () => {
   return (
     <div>
-      <Theme>
         <Routes />
         <Navbar />
-      </Theme>
     </div>
   );
 };
