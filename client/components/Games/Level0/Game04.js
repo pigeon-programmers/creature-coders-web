31f0618ff4732753acad71df481bea8d4a47e6b4
Caitlin Pauze Sherman <caitlinpauzesherman@Caitlins-MacBook-Pro.local>
2022-05-17 20:21:06 -0400

    adding changes to z-index in game 4 from code review

--- client/components/Games/Level0/Game04.js
+++ client/components/Games/Level0/Game04.js
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Workspace from "../Workspace";
+import React, { useState, useEffect } from 'react';
+import Workspace from '../Workspace';
 import {
   GameContent,
   Main,
   PopContainer,
   PopButton,
   Content,
-} from "../../style";
-import PopUp from "../../PopUp";
-import TryAgain from "../../TryAgain";
-import Interpreter from "js-interpreter";
-import "../Blocks/04Blocks";
+} from '../../style';
+import PopUp from '../../PopUp';
+import TryAgain from '../../TryAgain';
+import Interpreter from 'js-interpreter';
+import '../Blocks/04Blocks';
+import styled from 'styled-components';
+
+const GameWrapper = styled.div`
+  .blocklyToolboxDiv {
+    z-index: 5;
+  }
+`;
 
 export const Game04 = () => {
-  const [string, setString] = useState("");
-  const [number, setNumber] = useState("");
-  const [boolean, setBoolean] = useState("");
-  const [nullBlock, setNullBlock] = useState("");
-  const [object, setObject] = useState("");
-  const [undef, setUndef] = useState("");
+  const [string, setString] = useState('');
+  const [number, setNumber] = useState('');
+  const [boolean, setBoolean] = useState('');
+  const [nullBlock, setNullBlock] = useState('');
+  const [object, setObject] = useState('');
+  const [undef, setUndef] = useState('');
   const [mission, setMission] = useState(true);
   const [hint, setHint] = useState(false);
   const [tryAgain, setTryAgain] = useState(false);
@@ -26,76 +33,76 @@ export const Game04 = () => {
   useEffect(() => {
     // All types of state need to be added below for game to function properly!
     if (
-      string !== "" ||
-      number !== "" ||
-      boolean !== "" ||
-      nullBlock !== "" ||
-      object !== "" ||
-      undef !== ""
+      string !== '' ||
+      number !== '' ||
+      boolean !== '' ||
+      nullBlock !== '' ||
+      object !== '' ||
+      undef !== ''
     )
       outcome();
   }, [string, number, boolean, nullBlock, object, undef]);
 
   const toolbox = {
-    kind: "categoryToolbox",
+    kind: 'categoryToolbox',
     contents: [
       {
-        kind: "category",
-        name: "Types",
+        kind: 'category',
+        name: 'Types',
         contents: [
           {
-            kind: "block",
-            type: "string",
+            kind: 'block',
+            type: 'string',
           },
           {
-            kind: "block",
-            type: "boolean",
+            kind: 'block',
+            type: 'boolean',
           },
           {
-            kind: "block",
-            type: "number",
+            kind: 'block',
+            type: 'number',
           },
           {
-            kind: "block",
-            type: "undefined",
+            kind: 'block',
+            type: 'undefined',
           },
           {
-            kind: "block",
-            type: "object",
+            kind: 'block',
+            type: 'object',
           },
           {
-            kind: "block",
-            type: "null",
+            kind: 'block',
+            type: 'null',
           },
         ],
       },
       {
-        kind: "category",
-        name: "Examples",
+        kind: 'category',
+        name: 'Examples',
         contents: [
           {
-            kind: "block",
-            type: "string_ex",
+            kind: 'block',
+            type: 'string_ex',
           },
           {
-            kind: "block",
-            type: "number_ex",
+            kind: 'block',
+            type: 'number_ex',
           },
           {
-            kind: "block",
-            type: "boolean_ex",
+            kind: 'block',
+            type: 'boolean_ex',
           },
           {
-            kind: "block",
-            type: "null_ex",
+            kind: 'block',
+            type: 'null_ex',
           },
           {
-            kind: "block",
-            type: "object_ex",
+            kind: 'block',
+            type: 'object_ex',
           },
           {
-            kind: "block",
-            type: "undefined_ex",
+            kind: 'block',
+            type: 'undefined_ex',
           },
         ],
       },
@@ -105,13 +112,13 @@ export const Game04 = () => {
   const initApi = (interpreter, scope) => {
     const prop = (varName) => {
       const wrapper = function (text) {
-        text = text ? text.toString() : "";
-        if (varName === "string") setString(text);
-        if (varName === "number") setNumber(text);
-        if (varName === "boolean") setBoolean(text);
-        if (varName === "nullBlock") setNullBlock(text);
-        if (varName === "object") setObject(text);
-        if (varName === "undef") setUndef(text);
+        text = text ? text.toString() : '';
+        if (varName === 'string') setString(text);
+        if (varName === 'number') setNumber(text);
+        if (varName === 'boolean') setBoolean(text);
+        if (varName === 'nullBlock') setNullBlock(text);
+        if (varName === 'object') setObject(text);
+        if (varName === 'undef') setUndef(text);
       };
 
       interpreter.setProperty(
@@ -121,12 +128,12 @@ export const Game04 = () => {
       );
     };
 
-    prop("string");
-    prop("boolean");
-    prop("number");
-    prop("nullBlock");
-    prop("undef");
-    prop("object");
+    prop('string');
+    prop('boolean');
+    prop('number');
+    prop('nullBlock');
+    prop('undef');
+    prop('object');
   };
 
   const onRun = (javascriptCode) => {
@@ -135,13 +142,13 @@ export const Game04 = () => {
   };
 
   const outcome = () => {
-    string === "string" &&
-    boolean === "boolean" &&
-    number === "number" &&
-    nullBlock === "nullBlock" &&
-    undef === "undef" &&
-    object === "object"
-      ? alert("good job!")
+    string === 'string' &&
+    boolean === 'boolean' &&
+    number === 'number' &&
+    nullBlock === 'nullBlock' &&
+    undef === 'undef' &&
+    object === 'object'
+      ? alert('good job!')
       : setTryAgain(true);
   };
 
@@ -183,7 +190,9 @@ export const Game04 = () => {
           <TryAgain tryAgain={tryAgain} setTryAgain={setTryAgain} />
         </PopContainer>
         <GameContent></GameContent>
-        <Workspace toolbox={toolbox} onRun={onRun} />
+        <GameWrapper>
+          <Workspace toolbox={toolbox} onRun={onRun} />
+        </GameWrapper>
       </Content>
     </Main>
   );
---
+++ client/components/PetInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './style'
+
+const PetInfo = () => {
+
+  return(
+    <Main></Main>
+  )
+}
+
+export default PetInfo;
