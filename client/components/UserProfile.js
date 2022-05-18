c0db516fcb23d13eba48673c7d3d90579c213ae9
Kendall Perry <perry.kendall@gmail.com>
2022-05-18 15:10:39 -0400

    added route to get users pet, working route

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Main, Button, HomeTitle, HomeSubTitle } from './style';
-import styled from 'styled-components';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Main, Button, HomeTitle, HomeSubTitle, Content } from "./style";
+import styled from "styled-components";
 
 const UserBG = styled(Main)`
-background-color: #ED1697;
+  background-color: #ed1697;
+`;
+
+const UserContent = styled(Content)`
+background-color: rgba(126, 126, 126, 0.6);
+height: 70%;
+width: 80%;
 `
+
 const ProfileText = styled(HomeSubTitle)`
-font-size: 3vh;
-`
+  font-size: 3vh;
+  z-index: 50;
+`;
 
 const UserProfile = () => {
-    const { currentLevel, currentGame, username, badges, id } = useSelector((state) => state.user);
-    // const user = useSelector((state) => state.user);
+  const { currentLevel, currentGame, username, badges, id } = useSelector(
+    (state) => state.user
+  );
+  // const user = useSelector((state) => state.user);
 
-    // const isLoggedIn = useSelector((state) => !!state.auth.id);
+  // const isLoggedIn = useSelector((state) => !!state.auth.id);
 
-    return (
-        <UserBG>
-            <HomeTitle>{username}</HomeTitle>
-            <ProfileText>Current Level: {currentLevel}</ProfileText> 
-            <ProfileText>Current Game: {currentGame}</ProfileText>
-            <ProfileText>Badges: {badges ? badges.map((badge) => {
-                <ProfileText key={badge.id}>{badge.name}</ProfileText>
-            }) : null }</ProfileText>
-        </UserBG>
-    )
-}
+  return (
+    <UserBG>
+      <UserContent>
+        <HomeTitle>{username}</HomeTitle>
+        <ProfileText>Pet:</ProfileText>
+        <ProfileText>Current Level: {currentLevel}</ProfileText>
+        <ProfileText>Current Game: {currentGame}</ProfileText>
+        <ProfileText>
+          Badges:{" "}
+          {badges
+            ? badges.map((badge) => {
+                <ProfileText key={badge.id}>{badge.name}</ProfileText>;
+              })
+            : null}
+        </ProfileText>
+      </UserContent>
+    </UserBG>
+  );
+};
 
 export default UserProfile;
