f56e7c56648033f60ad42de7ef6aae7733f540ea
Kendall Perry <perry.kendall@gmail.com>
2022-05-18 15:56:58 -0400

    basic profile working, need to come back to badges section

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -2,17 +2,17 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Main, Button, HomeTitle, HomeSubTitle, Content } from "./style";
 import styled from "styled-components";
-import { getPet } from '../store/pet';
+import { getPet } from "../store/pet";
 
 const UserBG = styled(Main)`
   background-color: #ed1697;
 `;
 
 const UserContent = styled(Content)`
-background-color: rgba(126, 126, 126, 0.6);
-height: 70%;
-width: 80%;
-`
+  background-color: rgba(126, 126, 126, 0.6);
+  height: 70%;
+  width: 80%;
+`;
 
 const ProfileText = styled(HomeSubTitle)`
   font-size: 3vh;
@@ -22,31 +22,28 @@ const ProfileText = styled(HomeSubTitle)`
 const UserProfile = () => {
   const dispatch = useDispatch();
   const { currentLevel, currentGame, username, badges, id } = useSelector(
-    (state) => state.user
-  );
-  const pet = useSelector((state) => state.pet);
+(state) => state.user);
+  const { name, type } = useSelector((state) => state.pet);
 
   useEffect(() => {
-      if (id) dispatch(getPet(id));
-  }, [id])
-  console.log("PET INFO!", pet, id)
-  // const isLoggedIn = useSelector((state) => !!state.auth.id);
+    if (id) dispatch(getPet(id));
+  }, [id]);
 
   return (
     <UserBG>
       <UserContent>
         <HomeTitle>{username}</HomeTitle>
-        <ProfileText>Pet:</ProfileText>
+        <ProfileText>
+          Pet: {name} the {type}
+        </ProfileText>
         <ProfileText>Current Level: {currentLevel}</ProfileText>
         <ProfileText>Current Game: {currentGame}</ProfileText>
-        <ProfileText>
+        {/* <ProfileText>
           Badges:{" "}
-          {badges
-            ? badges.map((badge) => {
-                <ProfileText key={badge.id}>{badge.name}</ProfileText>;
-              })
-            : null}
         </ProfileText>
+         {badges && badges.length
+            ? badges.map((badge) => <ProfileText key={badge.id}>{badge.name}</ProfileText>)
+            : null} */}
       </UserContent>
     </UserBG>
   );
