669b73821f7d304e18dee40751dd84e2b196aea2
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-05-19 00:21:06 -0400

    everyone is dancing on the home page!

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,6 +3,10 @@ import { Main, Button, HomeTitle, HomeSubTitle } from './style';
 import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 
+const MainBG = styled(Main)`
+  background-color: #2828ff;
+`;
+
 const HomeButton = styled(Button)`
   width: 40vw;
   height: 7vh;
@@ -21,54 +25,60 @@ const SubTitle = styled(HomeSubTitle)`
   top: 25vh;
 `;
 
-const MainBG = styled(Main)`
-  background-color: #2828ff;
+const blinker = keyframes`
+0% {opacity: 0;}
+49.9% {opacity: 0;}
+50% {opacity: 1;}
+100% {opacity: 1;}
 `;
 
-const blinker = keyframes`
-0% {
-  opacity: 0;
-}
-49.9% {
-  opacity: 0;
-}
-50% {
-  opacity: 1;
-}
-100% {
-  opacity: 1;
-}
-`;
 const blinker2 = keyframes`
-0% {
-  opacity: 1;
-}
-49.9% {
-  opacity: 1;
-}
-50% {
-  opacity: 0;
-}
-100% {
-  opacity: 0;
-}
+0% {opacity: 1;}
+49.9% {opacity: 1;}
+50% {opacity: 0;}
+100% {opacity: 0;}
 `;
 
 const Bang = styled.img`
-  width: 95vw;
-  position: absolute;
-  top: 10vh;
-  animation: ${blinker} 1s linear infinite;
+width: 95vw;
+position: absolute;
+top: 10vh;
+animation: ${blinker} 1s linear infinite;
 `;
 
 const Bang2 = styled(Bang)`
-animation: ${blinker2} 1s linear infinite;
-`
+  animation: ${blinker2} 1s linear infinite;
+  `;
+
+  const twister = keyframes`
+  0% {transform: rotate(0);}
+  49.9% {transform: rotate(0);}
+  50% {transform: rotate(0.2turn);}
+  100% {transform: rotate(0.2turn);}
+  `;
 
 const SmallBang = styled.img`
-  width: 6vw;
+  width: 9vw;
   position: absolute;
+  animation: ${twister} 1s linear infinite;
+  top: 52vh;
+  left: 13vw;
 `;
+
+const SmallBang2 = styled(SmallBang)`
+  top: 10vh;
+  left: 75vw;
+`
+const SmallBang3 = styled(SmallBang)`
+  top: 67vh;
+  left: 86vw;
+  width: 12vw;
+`
+const SmallBang4 = styled(SmallBang)`
+  top: 81vh;
+  left: 19vw;
+  width: 6vw;
+`
 
 const BlocklyLogo = styled.img`
   width: 20vw;
@@ -97,9 +107,9 @@ const Home = (props) => {
       <Bang src="https://creature-coders.s3.amazonaws.com/bang1.svg" />
       <Bang2 src="https://creature-coders.s3.amazonaws.com/bang2.svg" />
       <SmallBang src="https://creature-coders.s3.amazonaws.com/bangSmall-01.svg" />
-      <SmallBang src="https://creature-coders.s3.amazonaws.com/bangSmall-02.svg" />
-      <SmallBang src="https://creature-coders.s3.amazonaws.com/bangSmall-03.svg" />
-      <SmallBang src="https://creature-coders.s3.amazonaws.com/bangSmall-04.svg" />
+      <SmallBang2 src="https://creature-coders.s3.amazonaws.com/bangSmall-02.svg" />
+      <SmallBang3 src="https://creature-coders.s3.amazonaws.com/bangSmall-03.svg" />
+      <SmallBang4 src="https://creature-coders.s3.amazonaws.com/bangSmall-04.svg" />
       <Title>
         {!mustLogIn ? 'Creature Coders' : 'Want to keep the fun going...?'}
       </Title>
