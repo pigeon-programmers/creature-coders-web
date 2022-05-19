153ec83e6b46f3f16665f3fb80fc41474408c18c
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-05-19 18:16:26 -0400

    hat styling looks good

--- client/components/Shop.js
+++ client/components/Shop.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { HomeTitle, Main } from './style';
+import { Button, HomeSubTitle, HomeTitle, Main } from './style';
 import styled from 'styled-components';
 
 const MainBg = styled(Main)`
   background-color: #ffe600;
 `;
 
+const Title = styled(HomeTitle)`
+  margin: 0;
+`;
+const SubTitle = styled(HomeSubTitle)`
+  color: black;
+`;
+
 const Hat = styled.img`
-  width: 10%;
+  width: 20%;
+  justify-content: space-around;
+  margin: 1em 3em;
+`;
+
+const HatButton = styled(Button)`
+  width: 30vw;
 `;
 
 const HatContainer = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const HatContainer2 = styled.div`
-display: flex;
-flex-direction: row;
-font-family: 'Anonymous Pro', 'Andale Mono', monospace;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: 'Anonymous Pro', 'Andale Mono', monospace;
+`;
 
 const Shop = () => {
   const allHats = [
@@ -49,7 +67,7 @@ const Shop = () => {
     },
     {
       name: 'tophat',
-      url: 'https://creature-coders.s3.amazonaws.com/hat-tophat.svg',
+      url: 'https://creature-coders.s3.amazonaws.com/hat-top.svg',
       cost: 1000000,
     },
   ];
@@ -57,23 +75,27 @@ const Shop = () => {
   return (
     <div>
       <MainBg>
-        <HatContainer>
+        <Title>Hat Shop!</Title>
+        <SubTitle>Buy hats with PidgeCoin</SubTitle>
 
-        {allHats.map((hat, index) => (
-          <HatContainer2 key={index}>
+        <HatContainer>
+          {allHats.map((hat, index) => (
+            <HatContainer2 key={index}>
               <Hat src={hat.url} />
-              <h3>{hat.cost}</h3>
-        </HatContainer2>
-        ))}
+              <HatButton>{`P ${hat.cost}`}</HatButton>
+            </HatContainer2>
+          ))}
         </HatContainer>
       </MainBg>
     </div>
   );
 };
 
-{/* <HatContainer>
+{
+  /* <HatContainer>
 <Hat key={index} src={hat.url} />
 <h3>{`$${hat.cost}`}</h3>
-</HatContainer> */}
+</HatContainer> */
+}
 
 export default Shop;
