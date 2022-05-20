df7fda02a59ed3fd989a641fe88f2b2f5eac6c8c
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-05-20 19:35:33 -0400

    loading component now works!

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Content, HomeTitle, palette } from './style';
+import styled, { keyframes } from 'styled-components';
+
+const Title = styled(HomeTitle)`
+  font-family: 'Andale Mono', monospace;
+  color: ${palette.mdGray};
+`;
+const spin = keyframes`
+  from {transform: rotate(0);}
+  to {transform: rotate(1turn);}
+`;
+const PidgeHead = styled.img`
+  animation: ${spin} 1s linear infinite;
+`;
+const LoadingContent = styled(Content)`
+  justify-content: space-evenly;
+`;
 
 const Loading = () => {
-    return (
-        <div>I am loading!</div>
-    )
+  return (
+    <LoadingContent>
+      <Title>loading...!</Title>
+      <PidgeHead src="pidgeheadbig.png" />
+    </LoadingContent>
+  );
 };
 
 export default Loading;
