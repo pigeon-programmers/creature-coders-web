46976cbc6907fd4b868609694468435a50f7a1d7
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-05-21 08:54:59 -0400

    Change game won text at bottom

--- client/Routes.js
+++ client/Routes.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { me } from "./store";
-import { getSingleUser } from "./store/user";
-import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
-import UserSettings from "./components/UserSettings";
-import UserProfile from "./components/UserProfile";
-import LeaderBoard from "./components/LeaderBoard";
-import Map from "./components/Map";
-import PetInfo from "./components/PetInfo";
-import NotFound from "./components/NotFound";
-import Game00 from "./components/Games/Level0/Game00";
-import Game01 from "./components/Games/Level0/Game01";
-import Game10 from "./components/Games/Level1/Game10";
-import Game11 from "./components/Games/Level1/Game11";
-import Game12 from "./components/Games/Level1/Game12";
-import Game20 from "./components/Games/Level2/Game20";
-import Game30 from "./components/Games/Level3/Game30";
-import GameWon from "./components/GameWon";
-import FAQ from "./components/FAQ";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { me } from './store';
+import { getSingleUser } from './store/user';
+import { Login, Signup } from './components/AuthForm';
+import Home from './components/Home';
+import UserSettings from './components/UserSettings';
+import UserProfile from './components/UserProfile';
+import LeaderBoard from './components/LeaderBoard';
+import Map from './components/Map';
+import PetInfo from './components/PetInfo';
+import NotFound from './components/NotFound';
+import Game00 from './components/Games/Level0/Game00';
+import Game01 from './components/Games/Level0/Game01';
+import Game10 from './components/Games/Level1/Game10';
+import Game11 from './components/Games/Level1/Game11';
+import Game12 from './components/Games/Level1/Game12';
+import Game20 from './components/Games/Level2/Game20';
+import Game30 from './components/Games/Level3/Game30';
+import GameWon from './components/GameWon';
+import EndGame from './components/EndGame';
+import FAQ from './components/FAQ';
 
-// TODO: add ternary for not found and loading, pull from redux state 
+// TODO: add ternary for not found and loading, pull from redux state
 
 const Routes = () => {
   const dispatch = useDispatch();
@@ -37,26 +38,27 @@ const Routes = () => {
   }, [isLoggedIn]);
 
   return (
-      <Switch>
-        <Route path="/" exact component={isLoggedIn ? Map : Home} />
-        <Route path="/map" component={Map} />
-        <Route path="/leaderboard" component={LeaderBoard} />
-        <Route path="/game/won" component={GameWon} />
-        <Route path="/game/0/0" exact component={Game00} />
-        <Route path="/game/0/1" exact component={Game01} />
-        <Route path="/game/1/0" exact component={Game10} />
-        <Route path="/game/1/1" exact component={Game11} />
-        <Route path="/game/1/2" exact component={Game12} />
-        <Route path="/game/2/0" exact component={Game20} />
-        <Route path="/game/3/0" exact component={Game30} />
-        <Route path="/faq" component={FAQ} />
-        {isLoggedIn && <Route path="/settings" component={UserSettings} />}
-        {isLoggedIn && <Route path="/profile" component={UserProfile} />}
-        {isLoggedIn && <Route path="/pet" component={PetInfo} />}
-        {!isLoggedIn && <Route path="/login" exact component={Login} />}
-        {!isLoggedIn && <Route path="/signup" exact component={Signup} />}
-        <Route component={NotFound} />
-      </Switch>
+    <Switch>
+      <Route path="/" exact component={isLoggedIn ? Map : Home} />
+      <Route path="/map" component={Map} />
+      <Route path="/leaderboard" component={LeaderBoard} />
+      <Route path="/game/won" component={GameWon} />
+      <Route path="/game/0/0" exact component={Game00} />
+      <Route path="/game/0/1" exact component={Game01} />
+      <Route path="/game/1/0" exact component={Game10} />
+      <Route path="/game/1/1" exact component={Game11} />
+      <Route path="/game/1/2" exact component={Game12} />
+      <Route path="/game/2/0" exact component={Game20} />
+      <Route path="/game/3/0" exact component={Game30} />
+      <Route path="/faq" component={FAQ} />
+      {isLoggedIn && <Route path="/settings" component={UserSettings} />}
+      {isLoggedIn && <Route path="/profile" component={UserProfile} />}
+      {isLoggedIn && <Route path="/pet" component={PetInfo} />}
+      {isLoggedIn && <Route path="/game/end" component={EndGame} />}
+      {!isLoggedIn && <Route path="/login" exact component={Login} />}
+      {!isLoggedIn && <Route path="/signup" exact component={Signup} />}
+      <Route component={NotFound} />
+    </Switch>
   );
 };
 
